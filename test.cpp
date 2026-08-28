50149bf9520465c51c59d9c4d7f43a1634812502
DvdRivas <davidadalrivasavila@gmail.com>
2026-08-27 21:30:17 -0600

    .

---
+++ test.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <vector> 
+#include <numeric>
+
+int main() {
+    
+    int i = 0;
+
+    for (;;) {
+        i++;
+        std::cout << "iteracion #" << i << "\n";
+    }
+}
