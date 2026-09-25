af8ad8847fdf7fb4abe680dfebce72636110952a
DvdRivas <davidadalrivasavila@gmail.com>
2026-09-24 23:05:02 -0600

    .

--- test.cpp
+++ test.cpp
@@ -1,16 +1,17 @@
 #include <iostream>
-#include <numeric>
-
 using namespace std;
 
-int main() {
-   for (int i = 1; i < 5; i++) {       // 'i' Externa
-    for (int i = 5; i >= 1; i--) {   // 'i' Interna (Redeclarada)
-        cout<< i << " ";
-     }
-    cout<< endl;
-     
+bool esPrimo(int n) {
+    if (n < 2) return false;          // 0 y 1 no son primos
+    if (n % 2 == 0) return n == 2;    // el único par primo es 2
+    for (int d = 3; d * d <= n; d += 2) {
+        if (n % d == 0) return false;
     }
-    cout << std::endl;
-    
+    return true;
+}
+
+int main() {
+    for (int i = 0; i <= 200; i++) {
+        cout << "el numero: " << i << " es: " << esPrimo(i) << endl;
+    }
 }
